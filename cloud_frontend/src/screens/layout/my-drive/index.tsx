34b438a8ffdf4e18a30c368a98a67db9a468c44d
Apoorv Saxena <apoorv.saxena@thewitslab.com>
2024-06-05 22:59:11 +0530

    add header in content

--- cloud_frontend/src/screens/layout/my-drive/index.tsx
+++ cloud_frontend/src/screens/layout/my-drive/index.tsx
@@ -1,14 +1,74 @@
-import { Box, Typography } from '@mui/material'
-import React from 'react'
+import "./style.scss";
+import { Grid, IconButton, Typography } from '@mui/material';
+import InsertDriveFileIcon from '@mui/icons-material/InsertDriveFile';
+import ContentHeader from '../../../components/content-header';
+import CloseIcon from '@mui/icons-material/Close';
+import DownloadIcon from '@mui/icons-material/Download';
 
 const MyDrive = () => {
     return (
         <div>
-            <Box className='center'>
-                <Typography variant="h6">Content</Typography>
+            <ContentHeader
+                title='My Drive'
+                viewMode="grid"
+                onSelectViewMode={(mode) => console.log(mode)}
+            />
 
-                <div className="action">action btn</div>
-            </Box>
+            <div className="file-actions">
+                <IconButton><CloseIcon /></IconButton>
+                <Typography variant="body2">2 Selected</Typography>
+                <IconButton className="ml-3"><DownloadIcon /></IconButton>
+            </div>
+
+            <div>
+                <Grid container spacing={2}>
+                    <Grid item xs={6} md={4} lg={3} xl={2}>
+                        <div className='file-card'>
+                            <div className="header">
+                                <InsertDriveFileIcon className="mr-3" fontSize="small" />
+                                <div className="title"> nfmsf sd sdffd fdg dfg fdg fdg fd g dg fd gd fg fd gd fgdf gdfg </div>
+                            </div>
+                            <div className="preview">
+                                preview
+                            </div>
+                        </div>
+                    </Grid>
+                    <Grid item xs={6} md={4} lg={3} xl={2}>
+                        <div className='file-card'>
+                            <div className="header">
+                                <InsertDriveFileIcon className="mr-3" fontSize="small" />
+                                <div className="title"> nfmsf sd sdffd fdg dfg fdg fdg fd g dg fd gd fg fd gd fgdf gdfg </div>
+                            </div>
+                            <div className="preview">
+                                preview
+                            </div>
+                        </div>
+                    </Grid>
+                    <Grid item xs={6} md={4} lg={3} xl={2}>
+                        <div className='file-card'>
+                            <div className="header">
+                                <InsertDriveFileIcon className="mr-3" fontSize="small" />
+                                <div className="title"> nfmsf sd sdffd fdg dfg fdg fdg fd g dg fd gd fg fd gd fgdf gdfg </div>
+                            </div>
+                            <div className="preview">
+                                preview
+                            </div>
+                        </div>
+                    </Grid>
+                    <Grid item xs={6} md={4} lg={3} xl={2}>
+                        <div className='file-card'>
+                            <div className="header">
+                                <InsertDriveFileIcon className="mr-3" fontSize="small" />
+                                <div className="title"> nfmsf sd sdffd fdg dfg fdg fdg fd g dg fd gd fg fd gd fgdf gdfg </div>
+                            </div>
+                            <div className="preview">
+                                preview
+                            </div>
+                        </div>
+                    </Grid>
+                </Grid>
+            </div>
+
         </div>
     )
 }
